70b5fed6c9a7fa45bc4340423ceb46b12272dd03
P Hewson <PVHewson@users.noreply.github.com>
2025-09-17 10:21:12 +1200

    Update index.js
    Extend string object with test for acceptable (Latin) characters

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ String.prototype.StripBraces = function ()
     return this.replace(/[{}]/g, "").toLowerCase();
 }
 
+/**
+ * Checks if the string contains only Latin characters, punctuation, numbers, symbols, separators, and optionally emojis.
+ * @param {any} allowEmoji
+ * @returns
+ */
+String.prototype.IsLatin = function (allowEmoji=false) {
+  if (this.length === 0) {
+    return true; // Empty string is considered valid
+  }
+  const emojiRegex = /[\u{1F600}-\u{1F64F}]/u
+  if (emojiRegex.test(this) && !allowEmoji) {
+    return false; // Contains emoji, not valid
+  }
+
+  return /^[\p{Script=Latin}\p{General_Category=Punctuation}\p{General_Category=Number}\p{General_Category=Symbol}\p{General_Category=Separator}\n]+$/u.test(this);
+}
+
 export {FormClass, RibbonClass, XrmUtilityClass, XrmWebApiClass};
